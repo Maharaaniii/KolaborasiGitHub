d7bcefef50a5d3a9e2a6b1473ecd509a61eae137
Maharaaniii <umimaharani08@gmail.com>
2026-03-14 23:01:34 +0700

    update login dan signup page

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const LoginPage()),
+        MaterialPageRoute(builder: (context) => LoginPage()),
       );
     });
   }
